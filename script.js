11cbe6bea0be6a345ecd3cff0e17b8703cb58839
Kamil Lewandowski <kamlew555@gmail.com>
2023-09-03 12:53:14 +0200

    create function formSubmit

--- script.js
+++ script.js
@@ -9,11 +9,24 @@ const confirmationEmailEl = document.querySelector(".newsletter__email");
 const dismissBtnEl = document.querySelector(".newsletter__dismiss__btn");
 const newsletterContainerEl = document.querySelector(".newsletter__container");
 
-
 function isValidEmail(email) {
 	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	return regex.test(email);
 }
 
+function handleSubmit(e) {
+	e.preventDefault();
+	const email = emailInputEl.value.trim();
+	if (isValidEmail(email)) {
+		errorMessageEl.style.visibility = "hidden";
+		confirmationEmailEl.textContent = email;
+		popupEl.style.display = "block";
+		newsletterContainerEl.style.display = "none";
+	} else {
+		errorMessageEl.style.visibility = "visible";
+		formEl.classList.add("error");
+		popupEl.style.display = "none";
+	}
+}
 
 formEl.addEventListener("submit", handleSubmit);
