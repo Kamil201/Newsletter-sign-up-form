d322b1d6da815654ced6c625279dd05a5cefddf7
Kamil Lewandowski <kamlew555@gmail.com>
2023-09-03 12:48:25 +0200

    grab into DOM elements

---
+++ script.js
@@ -0,0 +1,11 @@
+const formEl = document.querySelector(".newsletter__form__control");
+const errorMessageEl = document.querySelector(".newsletter__error__message");
+const emailInputEl = document.querySelector("#email");
+
+const popupEl = document.querySelector(".newsletter__popup__success");
+
+const confirmationEmailEl = document.querySelector(".newsletter__email");
+
+const dismissBtnEl = document.querySelector(".newsletter__dismiss__btn");
+const newsletterContainerEl = document.querySelector(".newsletter__container");
+
